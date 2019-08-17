9028ac66b3798b386c3bf369a5444ee6e2d9e92d
Igor Kalinovskiy <jolly-good-fellow@yandex.ru>
2019-08-18 02:41:11 +1000

    Main index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+import createSagaMiddleware from 'redux-saga'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './components/App'
+import reducer, { rootSaga } from 'state'
+
+const pReducer = persistReducer(
+  {
+    key: 'root',
+    storage
+  },
+  reducer
+)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(pReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+const persistor = persistStore(store)
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
